780ccda2f95e3c58f53bcd3a601a6c282d236d1c
Gonzalo Borderolle <gborderolle@gmail.com>
2024-01-06 01:09:32 -0300

    .

--- Buildyv2back/Utilities/AutoMapperProfiles.cs
+++ Buildyv2back/Utilities/AutoMapperProfiles.cs
@@ -7,23 +7,27 @@ namespace Buildyv2.Utilities
 {
     public class AutoMapperProfiles : Profile
     {
-        public AutoMapperProfiles()
+        private readonly IWebHostEnvironment _env;
+
+        public AutoMapperProfiles(IWebHostEnvironment env)
         {
+            _env = env;
+
             CreateMap<IdentityUser, UserDTO>();
 
             CreateMap<Estate, EstateDTO>()
-    .ForMember(dest => dest.CityDS, opt => opt.MapFrom(src => src.CityDS))
-                .ForMember(dest => dest.ListReports, opt => opt.MapFrom(src => src.ListReports))
-                .ForMember(dest => dest.ListJobs, opt => opt.MapFrom(src => src.ListJobs))
-                .ForMember(dest => dest.ListRents, opt => opt.MapFrom(src => src.ListRents))
-                .ReverseMap();
+              .ForMember(dest => dest.CityDS, opt => opt.MapFrom(src => src.CityDS))
+              .ForMember(dest => dest.ListReports, opt => opt.MapFrom(src => src.ListReports))
+              .ForMember(dest => dest.ListJobs, opt => opt.MapFrom(src => src.ListJobs))
+              .ForMember(dest => dest.ListRents, opt => opt.MapFrom(src => src.ListRents))
+              .ReverseMap();
             CreateMap<EstateCreateDTO, Estate>()
-                .ForMember(dest => dest.CityDS, opt => opt.Ignore()) // Ignorar este campo
-                .ReverseMap();
+              .ForMember(dest => dest.CityDS, opt => opt.Ignore()) // Ignorar este campo
+              .ReverseMap();
 
             CreateMap<WorkerCreateDTO, Worker>()
-    .ForMember(dest => dest.JobId, opt => opt.MapFrom(src => src.JobId.HasValue ? src.JobId.Value : (int?)null))
-    .ReverseMap();
+              .ForMember(dest => dest.JobId, opt => opt.MapFrom(src => src.JobId.HasValue ? src.JobId.Value : (int?)null))
+              .ReverseMap();
             CreateMap<Worker, WorkerDTO>().ReverseMap();
 
             CreateMap<TenantCreateDTO, Tenant>().ReverseMap();
@@ -33,57 +37,81 @@ namespace Buildyv2.Utilities
             CreateMap<CountryDSCreateDTO, CountryDS>().ReverseMap();
 
             CreateMap<ProvinceDS, ProvinceDSDTO>()
-                .ForMember(dest => dest.CountryDSId,
-                           opt => opt.MapFrom(src => src.CountryDS.Id))
-                .ReverseMap();
+              .ForMember(dest => dest.CountryDSId,
+                opt => opt.MapFrom(src => src.CountryDS.Id))
+              .ReverseMap();
             CreateMap<ProvinceDSCreateDTO, ProvinceDS>()
-                .ForMember(dest => dest.CountryDS, opt => opt.Ignore()) // Ignorar este campo
-                .ReverseMap();
+              .ForMember(dest => dest.CountryDS, opt => opt.Ignore()) // Ignorar este campo
+              .ReverseMap();
 
             CreateMap<CityDS, CityDSDTO>()
-                .ForMember(dest => dest.ProvinceDSId,
-                           opt => opt.MapFrom(src => src.ProvinceDS.Id))
-                .ReverseMap();
+              .ForMember(dest => dest.ProvinceDSId,
+                opt => opt.MapFrom(src => src.ProvinceDS.Id))
+              .ReverseMap();
             CreateMap<CityDSCreateDTO, CityDS>()
-            .ForMember(dest => dest.ProvinceDS, opt => opt.Ignore()) // Ignorar este campo
-        .ReverseMap();
+              .ForMember(dest => dest.ProvinceDS, opt => opt.Ignore()) // Ignorar este campo
+              .ReverseMap();
 
             CreateMap<Report, ReportDTO>().ReverseMap();
             CreateMap<ReportCreateDTO, Report>()
-    .ForMember(dest => dest.Id, opt => opt.Ignore()) // Ignorar Id ya que es generado por la base de datos
-                .ForMember(dest => dest.ListPhotos, opt => opt.MapFrom(src => MapIFormFilesToPhotos(src.ListPhotos)))
-    .ReverseMap();
+              .ForMember(dest => dest.Id, opt => opt.Ignore()) // Ignorar Id ya que es generado por la base de datos
+              .ForMember(dest => dest.ListPhotos, opt => opt.MapFrom(src => MapIFormFilesToPhotos(src.ListPhotos, src.Id, src.Creation)))
+              .ReverseMap();
 
             CreateMap<JobCreateDTO, Job>()
-    .ForMember(dest => dest.Id, opt => opt.Ignore()) // Ignorar Id ya que es generado por la base de datos
-    .ReverseMap();
+              .ForMember(dest => dest.Id, opt => opt.Ignore()) // Ignorar Id ya que es generado por la base de datos
+              .ReverseMap();
             CreateMap<Job, JobDTO>().ReverseMap();
 
         }
 
-        private List<Photo> MapIFormFilesToPhotos(List<IFormFile> files)
+        private List<Photo> MapIFormFilesToPhotos(List<IFormFile> files, int reportId, DateTime creationDate)
         {
             var photos = new List<Photo>();
+            int count = 1;
             foreach (var file in files)
             {
                 var photo = new Photo
                 {
-                    // Aquí debes definir cómo transformar un IFormFile en un Photo
-                    // Por ejemplo, podrías querer guardar el archivo en algún lugar y poner la URL en Photo.Url
-                    URL = SaveFileSomewhereAndGetUrl(file)
+                    URL = SaveFileAndGetUrl(file, reportId, creationDate, count)
                 };
                 photos.Add(photo);
+                count++;
             }
             return photos;
         }
 
-        private string SaveFileSomewhereAndGetUrl(IFormFile file)
+        private string SaveFileAndGetUrl(IFormFile file, int reportId, DateTime creationDate, int fileCount)
         {
-            // Implementa la lógica para guardar el archivo y devolver la URL
-            // Esto depende de cómo manejes la carga y almacenamiento de archivos en tu aplicación
-            throw new NotImplementedException();
-        }
+            if (file == null || file.Length == 0)
+            {
+                throw new ArgumentException("El archivo proporcionado es inválido.");
+            }
 
+            // Construye la ruta base usando el ReportId y la fecha de creación.
+            string basePath = Path.Combine(_env.WebRootPath, "/wwwroot/uploads/reports", reportId.ToString(), creationDate.ToString("yyyyMMddHHmmss"));
+
+            // Asegúrate de que la carpeta exista.
+            if (!Directory.Exists(basePath))
+            {
+                Directory.CreateDirectory(basePath);
+            }
+
+            // Construye un nombre de archivo que incluya el contador de archivos.
+            string fileName = fileCount.ToString() + "_" + file.FileName;
+
+            // Combina el nombre del archivo con la ruta del directorio.
+            string filePath = Path.Combine(basePath, fileName);
+
+            // Guarda el archivo en el sistema de archivos.
+            using (var fileStream = new FileStream(filePath, FileMode.Create))
+            {
+                file.CopyTo(fileStream);
+            }
+
+            // Retorna la ruta donde el archivo fue guardado.
+            return filePath;
+        }
 
     }
 }
