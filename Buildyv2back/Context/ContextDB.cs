32516c616f79f3dd58301fb397fe40b7765abd18
Gonzalo Borderolle <gborderolle@gmail.com>
2024-01-27 02:05:35 -0300

    Update package references and fix formatting in ContextDB.cs

--- Buildyv2back/Context/ContextDB.cs
+++ Buildyv2back/Context/ContextDB.cs
@@ -35,9 +35,9 @@ namespace Buildyv2.Context
             base.OnModelCreating(modelBuilder);
 
             modelBuilder.Entity<Report>()
-        .HasMany(r => r.ListPhotos)
-        .WithOne(p => p.Report)
-        .HasForeignKey(p => p.ReportId);
+                .HasMany(r => r.ListPhotos)
+                .WithOne(p => p.Report)
+                .HasForeignKey(p => p.ReportId);
 
             SeedData(modelBuilder);
         }
